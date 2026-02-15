f249c7187b6a25a842c6aa52352a542a3021f0b1
Christiaan van Attekum <d.v.attekum@gmail.com>
2026-02-15 15:14:04 +0100

    Add project files.

---
+++ Bussen/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Bussen
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Bussen/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Bussen
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
